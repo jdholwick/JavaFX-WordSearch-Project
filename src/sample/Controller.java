3ec0d2a3129ee82d5c9ff2445ab282b3f705a483
jdholwick <jdholwick@gmail.com>
2020-10-23 19:18:03 -0600

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void clickStartButton(ActionEvent event) {
+        System.out.println("Word Search will display in GUI later...");
+    }
+
+    public void clickExitButton(ActionEvent event) {
+        System.out.println("Word Search will exit when clicked...");
+        System.exit(0);
+    }
+}
+
+
+// The following is from 'Interface.fxml' and was altered once before destroying functionality, without my doing so, so I'm saving a copy of it here:
+/*
+<?xml version="1.0" encoding="UTF-8"?>
+
+<?import javafx.scene.control.*?>
+<?import javafx.scene.layout.*?>
+
+<GridPane alignment="center" hgap="10" prefHeight="300.0" prefWidth="300.0" vgap="10" xmlns="http://javafx.com/javafx/11.0.1" xmlns:fx="http://javafx.com/fxml/1" fx:controller="sample.Controller">
+   <columnConstraints>
+      <ColumnConstraints />
+   </columnConstraints>
+   <rowConstraints>
+      <RowConstraints />
+   </rowConstraints>
+   <children>
+      <Button fx:id="btnNotify" mnemonicParsing="false" onAction="#clickStartButton" text="Button" />
+   </children>
+</GridPane>
+
+ */
