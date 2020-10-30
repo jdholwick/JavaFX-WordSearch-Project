3682badd160cb55924ba1f63fd13b097025d47c1
jdholwick <jdholwick@gmail.com>
2020-10-30 12:08:17 -0600

    Presently working on changing the letters on the board when a label is clicked.

--- src/wordsearch/Controller.java
+++ src/wordsearch/Controller.java
@@ -48,29 +48,7 @@ public class Controller {
         String labelLetter = lbl_1_1.getText();
 
         changeLabel("LOOKING");
-        System.out.println("hi");
+        System.out.println("Presently just changes a column of letters to a new word when a letter label is clicked.");
 
     }
 }
-
-
-// The following is from 'Interface.fxml' and was altered once before destroying functionality, without my doing so, so I'm saving a copy of it here:
-/*
-<?xml version="1.0" encoding="UTF-8"?>
-
-<?import javafx.scene.control.*?>
-<?import javafx.scene.layout.*?>
-
-<GridPane alignment="center" hgap="10" prefHeight="300.0" prefWidth="300.0" vgap="10" xmlns="http://javafx.com/javafx/11.0.1" xmlns:fx="http://javafx.com/fxml/1" fx:controller="wordsearch.Controller">
-   <columnConstraints>
-      <ColumnConstraints />
-   </columnConstraints>
-   <rowConstraints>
-      <RowConstraints />
-   </rowConstraints>
-   <children>
-      <Button fx:id="btnNotify" mnemonicParsing="false" onAction="#clickStartButton" text="Button" />
-   </children>
-</GridPane>
-
- */
