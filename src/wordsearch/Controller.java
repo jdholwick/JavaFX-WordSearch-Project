ea1c22e3a815b78c84e99fca0e1148f0c8605230
jdholwick <jdholwick@gmail.com>
2020-11-01 07:19:49 -0700

    Presently working on developing the board letter schemes.

--- src/wordsearch/Controller.java
+++ src/wordsearch/Controller.java
@@ -8,19 +8,13 @@ import javafx.scene.input.MouseEvent;
 public class Controller {
 
     @FXML
-    private Label lbl_1_1;
+    private Label lbl_1_1, lbl_2_1, lbl_3_1, lbl_4_1, lbl_5_1, lbl_6_1, lbl_7_1, lbl_8_1, lbl_9_1, lbl_10_1, lbl_11_1, lbl_12_1, lbl_13_1, lbl_14_1, lbl_15_1;
     @FXML
-    private Label lbl_2_1;
+    private Label lbl_1_2, lbl_2_2, lbl_3_2, lbl_4_2, lbl_5_2, lbl_6_2, lbl_7_2, lbl_8_2, lbl_9_2, lbl_10_2, lbl_11_2, lbl_12_2, lbl_13_2, lbl_14_2, lbl_15_2;
     @FXML
-    private Label lbl_3_1;
+    private Label lbl_1_3, lbl_2_3, lbl_3_3, lbl_4_3, lbl_5_3, lbl_6_3, lbl_7_3, lbl_8_3, lbl_9_3, lbl_10_3, lbl_11_3, lbl_12_3, lbl_13_3, lbl_14_3, lbl_15_3;
     @FXML
-    private Label lbl_4_1;
-    @FXML
-    private Label lbl_5_1;
-    @FXML
-    private Label lbl_6_1;
-    @FXML
-    private Label lbl_7_1;
+    private Label lbl_1_4, lbl_2_4, lbl_3_4, lbl_4_4, lbl_5_4, lbl_6_4, lbl_7_4, lbl_8_4, lbl_9_4, lbl_10_4, lbl_11_4, lbl_12_4, lbl_13_4, lbl_14_4, lbl_15_4;
 
     public void clickStartButton(ActionEvent event) {
         System.out.println("Word Search will display in GUI later...");
@@ -39,6 +33,7 @@ public class Controller {
         lbl_5_1.setText(String.valueOf(newLabel.charAt(4)));
         lbl_6_1.setText(String.valueOf(newLabel.charAt(5)));
         lbl_7_1.setText(String.valueOf(newLabel.charAt(6)));
+        lbl_15_3.setText("A");
     }
 
     public void onClick(MouseEvent mouseEvent) {
